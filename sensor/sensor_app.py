22b6ac658411bf03ecd958dab353ad7501e81cc9
Bart Pilarczyk <b.m.pilarczykgmail.com>
2023-02-07 08:27:41 -0500

    Completed module

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -6,6 +6,7 @@ from temperature_info import TemperatureData
 from humidity_info import HumidityData
 from statistics import mean
 from particle_count_info import ParticleData
+from energy_info import EnergyData
 
 ##############################
 # Do not remove these two lines
@@ -73,3 +74,12 @@ print("\nHouse Partcile sensor records for date {} = {}".format(
 print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
+# Module 6 code here:
+energy_data = EnergyData(data)
+
+recs = energy_data.get_data_by_area(rec_area = test_area)
+print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
+
+total_energy = energy_data.calculate_energy_usage(data = recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
